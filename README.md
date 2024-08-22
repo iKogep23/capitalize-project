5a36977a7b271c387ccdbf563b4eb86232f8eadd
Igor Bessonov <ikogep23@gmail.com>
2024-08-22 15:57:00 +0300

    fix test_capitalize and upd. README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# capitalize-project
+## capitalize-project
 A program which capitalize first letter of the given text.
+This program is written as the first training package in the course on automated tests.
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,12 +1,14 @@
-from capitalize import capitalize
+from capitalize_package.capitalize import capitalize
+
+def main():
+    if capitalize('hello') != 'Hello':
+        raise Exception('Function does not work correctly!')
+
+    if capitalize('') != '':
+        raise Exception('Function does not work correctly!')
+
+    print('All tests passed')
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Function does not work correctly!')
-
-
-if capitalize('') != '':
-    raise Exception('Function does not work correctly!')
-
-
-print('All tests passed')
+if __name__ == '__main__':
+    main()
