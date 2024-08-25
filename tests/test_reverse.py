e2c300b741574a3d4e8596689f7e3a9ab9782458
Igor Bessonov <ikogep23@gmail.com>
2024-08-25 16:19:39 +0300

    add fixtures and test with them

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -7,3 +7,11 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+def test_fixture():
+    with_input_path = 'tests/fixtures/input.txt'
+    with_output_path = 'tests/fixtures/output.txt'
+    with open(with_input_path, encoding='utf8') as input_file:
+        result = reverse(input_file.read().strip())
+    with open(with_output_path, encoding='utf8') as output_file:
+        assert result == output_file.read().strip()
